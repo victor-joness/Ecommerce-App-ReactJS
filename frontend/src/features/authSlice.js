9de4cee54dd6b76ea10f8305dfb85b4006bf5609
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-02-09 09:17:02 -0300

    Create authSlice.js

---
+++ frontend/src/features/authSlice.js
@@ -0,0 +1,159 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { url } from "./api";
+import jwtDecode from "jwt-decode";
+
+const initialState = {
+  token: localStorage.getItem("token"),
+  name: "",
+  email: "",
+  _id: "",
+  isAdmin: "",
+  registerStatus: "",
+  registerError: "",
+  loginStatus: "",
+  loginError: "",
+  userLoaded: false,
+};
+
+//esse registerUser recebe esse createAsyncThunk, esse asynThunk é usado em funcoes assyncronas, e eu posso definir tipos para isso de acordo com o que ele me retorna
+//podendo ser pending,fulfilled,rejected e fazer alguma coisa de acorodo com esses retornos.
+export const registerUser = createAsyncThunk(
+  "auth/registerUser",
+  async (user, { rejectWithValue }) => {
+    try {
+      const token = await axios.post(`${url}/register`, {
+        name: user.name,
+        email: user.email,
+        senha: user.senha,
+      });
+
+      localStorage.setItem("token", token.data);
+      return token.data;
+    } catch (error) {
+      console.log(error.response.data);
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "auth/loginUser",
+  async (user, { rejectWithValue }) => {
+    try {
+      const token = await axios.post(`${url}/login`, {
+        email: user.email,
+        senha: user.senha,
+      });
+
+      localStorage.setItem("token", token.data);
+      return token.data;
+    } catch (error) {
+      console.log(error.response.data);
+      return rejectWithValue(error.response.data);
+    }
+  }
+);
+
+//aqui temos um "slice" do redux, aqui a gente pode definir um objeto e esse objeto pode ter varios estados(states), atraves do inicialState a genter define esse estados
+//temos tbm os reducers -> que pode ser traduzido como a onde fica os metodos que vão mexer nesses estados, aqui no caso não tem mais a gente pode ver isso claramento no cartSlice
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    loadUser(state, action) {
+      const token = state.token;
+      if (token) {
+        const user = jwtDecode(token);
+        return {
+          ...state,
+          token,
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+          isAdmin: user.isAdmin,
+          userLoaded: true,
+        };
+      }
+    },
+    logoutUser(state, action) {
+      localStorage.removeItem("token");
+
+      return {
+        ...state,
+        token: "",
+        name: "",
+        email: "",
+        _id: "",
+        registerStatus: "",
+        registerError: "",
+        loginStatus: "",
+        loginError: "",
+        userLoaded: false,
+      };
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(registerUser.pending, (state, action) => {
+      return { ...state, registerStatus: "pending" };
+    });
+
+    builder.addCase(registerUser.fulfilled, (state, action) => {
+      if (action.payload) {
+        const user = jwtDecode(action.payload);
+        return {
+          ...state,
+          token: action.payload,
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+          isAdmin: user.isAdmin,
+          registerStatus: "success",
+        };
+      } else {
+        return state;
+      }
+    });
+
+    builder.addCase(registerUser.rejected, (state, action) => {
+      return {
+        ...state,
+        registerStatus: "rejected",
+        registerError: action.payload,
+      };
+    });
+
+    builder.addCase(loginUser.pending, (state, action) => {
+      return { ...state, loginStatus: "pending" };
+    });
+
+    builder.addCase(loginUser.fulfilled, (state, action) => {
+      if (action.payload) {
+        const user = jwtDecode(action.payload);
+        return {
+          ...state,
+          token: action.payload,
+          name: user.name,
+          email: user.email,
+          _id: user._id,
+          isAdmin: user.isAdmin,
+          loginStatus: "success",
+        };
+      } else {
+        return state;
+      }
+    });
+
+    builder.addCase(loginUser.rejected, (state, action) => {
+      return {
+        ...state,
+        loginStatus: "rejected",
+        loginError: action.payload,
+      };
+    });
+  },
+});
+
+export const { loadUser, logoutUser } = authSlice.actions;
+
+export default authSlice.reducer;
