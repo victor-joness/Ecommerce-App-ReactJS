03dcd63b58c34ea007d69451f9e9613dcd054ccd
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-02-10 08:15:09 -0300

    Create NotFound.jsx

---
+++ frontend/src/Components/NotFound/NotFound.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./NotFound.css"
+
+const NotFound = () => {
+  return (
+    <div className='not-found'>
+        <h2>404</h2>
+        <p>Page not found</p>
+    </div>
+  )
+}
+
+export default NotFound
