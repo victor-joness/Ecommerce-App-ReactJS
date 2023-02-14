5a25c0eb483f7165f91fc61db53b850ffa0c7eb6
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-02-14 08:49:58 -0300

    Create Users.jsx

---
+++ frontend/src/Components/admin/Users/Users.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import UsersList from '../list/UsersList/UsersList';
+
+const Users = () => {
+  return (
+    <div>
+      <UsersList></UsersList>
+    </div>
+  )
+}
+
+export default Users;
