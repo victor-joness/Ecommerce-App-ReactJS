ffa47550cc1106ab0418b7f268dc8b84d6f13ce9
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-02-21 08:27:14 -0300

    Create Orders.jsx

---
+++ frontend/src/Components/admin/Orders/Orders.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import OrderList from "../list/OrderList/OrderList";
+
+const Orders = () => {
+  return (
+    <div>
+      <OrderList></OrderList>
+    </div>
+  );
+};
+
+export default Orders;
