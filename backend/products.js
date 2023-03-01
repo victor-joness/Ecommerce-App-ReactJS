45f46ca24adbce396ef85c8bf587fa5163f95e58
Victor Mesquita <victorjonesmesquits@gmail.com>
2023-03-01 08:18:08 -0300

    Create products.js

---
+++ backend/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        name : "iphone 11 pro",
+        desc: "6.1-inch display",
+        price: 999,
+        img: "https://res.cloudinary.com/daqtzt9b9/image/upload/v1674145090/OnlineShop/iphone12_xtk7ud.jpg"
+    },
+    {
+        id: 2,
+        name : "gaalxy",
+        desc: "6.1-inch display",
+        price: 699,
+        img: "https://res.cloudinary.com/daqtzt9b9/image/upload/v1674145090/OnlineShop/galaxyS_e63ike.png"
+    },
+    {
+        id: 3,
+        name : "iphone 13 pro",
+        desc: "6.1-inch display",
+        price: 199,
+        img: "https://res.cloudinary.com/daqtzt9b9/image/upload/v1674145090/OnlineShop/iphone12pro_vln7op.jpg"
+    },
+];
+
+module.exports = products;
